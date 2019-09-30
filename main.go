0843c6bca3144fc608f1890a7b73bf02bfaed924
Jorrit Salverda <jsalverda@travix.com>
2019-09-30 16:38:18 +0200

    allow adding a suffix to the version tag so multiple containers can be built and pushed from the same pipeline

--- main.go
+++ main.go
@@ -38,6 +38,7 @@ var (
 	target                     = kingpin.Flag("target", "Specify which stage to target for multi-stage build.").Envar("ESTAFETTE_EXTENSION_TARGET").String()
 	args                       = kingpin.Flag("args", "List of build arguments to pass to the build.").Envar("ESTAFETTE_EXTENSION_ARGS").String()
 	pushVersionTag             = kingpin.Flag("push-version-tag", "By default the version tag is pushed, so it can be promoted with a release, but if you don't want it you can disable it via this flag.").Default("true").Envar("ESTAFETTE_EXTENSION_PUSH_VERSION_TAG").Bool()
+	versionTagSuffix           = kingpin.Flag("version-tag-suffix", "A suffix to add to the version tag so promoting different containers originating from the same pipeline is possible.").Envar("ESTAFETTE_EXTENSION_VERSION_TAG_SUFFIX").String()
 	noCache                    = kingpin.Flag("no-cache", "Indicates cache shouldn't be used when building the image.").Default("false").Envar("ESTAFETTE_EXTENSION_NO_CACHE").Bool()
 	expandEnvironmentVariables = kingpin.Flag("expand-envvars", "By default environment variables get replaced in the Dockerfile, use this flag to disable that behaviour").Default("true").Envar("ESTAFETTE_EXTENSION_EXPAND_VARIABLES").Bool()
 
@@ -108,6 +109,9 @@ func main() {
 	}
 	estafetteBuildVersion := os.Getenv("ESTAFETTE_BUILD_VERSION")
 	estafetteBuildVersionAsTag := tidyTag(estafetteBuildVersion)
+	if *versionTagSuffix != "" {
+		estafetteBuildVersionAsTag += *versionTagSuffix
+	}
 	gitBranchAsTag := tidyTag(fmt.Sprintf("cache-%v", *gitBranch))
 
 	switch *action {
